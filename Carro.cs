b37b83fd5d31d538e5c895aedcb0b029fa9fd72e
LucasDanielBCCIFGoiano <88989934+LucasDanielBCCIFGoiano@users.noreply.github.com>
2021-08-23 00:58:41 -0300

    Adicionando arquivos

---
+++ Carro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorfismoCinara
+{
+    public class Carro : MeioTransporte
+    {
+        public string Marca;
+
+        public override void Mover()
+        {
+            Console.WriteLine($" {Marca} {Modelo} andando...");
+
+        }
+    }
+}
---
+++ MeioTransporte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorfismoCinara
+{
+    public class MeioTransporte
+    {
+
+        public string Modelo;
+        public virtual void Mover()
+        {
+
+
+        }
+    }
+}
